aec099ba20d692697b676a9f58a56cfb66467614
munir <Munir.salihzoy@gmail.com>
2025-03-16 20:49:11 +0100

    uitvoer werkt

---
+++ PSE/cmake-build-debug/CMakeFiles/release_target.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/munir/CLionProjects/Project-software-engineering/PSE/src/baan.cpp" "CMakeFiles/release_target.dir/src/baan.cpp.obj" "gcc" "CMakeFiles/release_target.dir/src/baan.cpp.obj.d"
+  "C:/Users/munir/CLionProjects/Project-software-engineering/PSE/src/main_release.cpp" "CMakeFiles/release_target.dir/src/main_release.cpp.obj" "gcc" "CMakeFiles/release_target.dir/src/main_release.cpp.obj.d"
+  "C:/Users/munir/CLionProjects/Project-software-engineering/PSE/src/simulatie.cpp" "CMakeFiles/release_target.dir/src/simulatie.cpp.obj" "gcc" "CMakeFiles/release_target.dir/src/simulatie.cpp.obj.d"
+  "C:/Users/munir/CLionProjects/Project-software-engineering/PSE/src/situatie.cpp" "CMakeFiles/release_target.dir/src/situatie.cpp.obj" "gcc" "CMakeFiles/release_target.dir/src/situatie.cpp.obj.d"
+  "C:/Users/munir/CLionProjects/Project-software-engineering/PSE/src/tinystr.cpp" "CMakeFiles/release_target.dir/src/tinystr.cpp.obj" "gcc" "CMakeFiles/release_target.dir/src/tinystr.cpp.obj.d"
+  "C:/Users/munir/CLionProjects/Project-software-engineering/PSE/src/tinyxml.cpp" "CMakeFiles/release_target.dir/src/tinyxml.cpp.obj" "gcc" "CMakeFiles/release_target.dir/src/tinyxml.cpp.obj.d"
+  "C:/Users/munir/CLionProjects/Project-software-engineering/PSE/src/tinyxmlerror.cpp" "CMakeFiles/release_target.dir/src/tinyxmlerror.cpp.obj" "gcc" "CMakeFiles/release_target.dir/src/tinyxmlerror.cpp.obj.d"
+  "C:/Users/munir/CLionProjects/Project-software-engineering/PSE/src/tinyxmlparser.cpp" "CMakeFiles/release_target.dir/src/tinyxmlparser.cpp.obj" "gcc" "CMakeFiles/release_target.dir/src/tinyxmlparser.cpp.obj.d"
+  "C:/Users/munir/CLionProjects/Project-software-engineering/PSE/src/verkeerslicht.cpp" "CMakeFiles/release_target.dir/src/verkeerslicht.cpp.obj" "gcc" "CMakeFiles/release_target.dir/src/verkeerslicht.cpp.obj.d"
+  "C:/Users/munir/CLionProjects/Project-software-engineering/PSE/src/voertuig.cpp" "CMakeFiles/release_target.dir/src/voertuig.cpp.obj" "gcc" "CMakeFiles/release_target.dir/src/voertuig.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ PSE/cmake-build-debug/CMakeFiles/release_target.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/release_target.dir/src/baan.cpp.obj"
+  "CMakeFiles/release_target.dir/src/baan.cpp.obj.d"
+  "CMakeFiles/release_target.dir/src/main_release.cpp.obj"
+  "CMakeFiles/release_target.dir/src/main_release.cpp.obj.d"
+  "CMakeFiles/release_target.dir/src/simulatie.cpp.obj"
+  "CMakeFiles/release_target.dir/src/simulatie.cpp.obj.d"
+  "CMakeFiles/release_target.dir/src/situatie.cpp.obj"
+  "CMakeFiles/release_target.dir/src/situatie.cpp.obj.d"
+  "CMakeFiles/release_target.dir/src/tinystr.cpp.obj"
+  "CMakeFiles/release_target.dir/src/tinystr.cpp.obj.d"
+  "CMakeFiles/release_target.dir/src/tinyxml.cpp.obj"
+  "CMakeFiles/release_target.dir/src/tinyxml.cpp.obj.d"
+  "CMakeFiles/release_target.dir/src/tinyxmlerror.cpp.obj"
+  "CMakeFiles/release_target.dir/src/tinyxmlerror.cpp.obj.d"
+  "CMakeFiles/release_target.dir/src/tinyxmlparser.cpp.obj"
+  "CMakeFiles/release_target.dir/src/tinyxmlparser.cpp.obj.d"
+  "CMakeFiles/release_target.dir/src/verkeerslicht.cpp.obj"
+  "CMakeFiles/release_target.dir/src/verkeerslicht.cpp.obj.d"
+  "CMakeFiles/release_target.dir/src/voertuig.cpp.obj"
+  "CMakeFiles/release_target.dir/src/voertuig.cpp.obj.d"
+  "librelease_target.dll.a"
+  "release_target.exe"
+  "release_target.exe.manifest"
+  "release_target.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/release_target.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
